2bdbbcdd114bcc5623f9c302554b2c3364829eed
Henrique Poleselo <hpoleselo@gmail.com>
2018-11-19 22:47:59 +0100

    Adding the camera interface along withe calibration pictures.

--- henrique/src/arucointerface.py
+++ henrique/src/arucointerface.py
@@ -9,21 +9,23 @@ import cv2.aruco as aruco
 class ArucoInterface(object):
 
     # If you want to see the pictures slowly then just change the WAIT_TIME
-    def __init__(self, WAIT_TIME=10 , chessb_col=8, chessb_row=6):
+    def __init__(self, wait_time=10 , chessb_col=9, chessb_row=6):
 
-        # Our chessboard is composed by 9 rows and 6 columns! Which only 8x6 corners would be detected
+        # Our chessboard is composed by 10 rows and 6 columns! Which only 9x6 corners would be detected
 
-        self.WAIT_TIME = WAIT_TIME
+        self.wait_time = wait_time
         self.chessb_col = chessb_col
         self.chessb_row = chessb_row
         # Path for loading the images
         self.load_image = 'calib_images/*.jpg'
         # termination criteria
         self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # Size of the ArUco marker in meters
+        self.marker_size = 0.10
 
     # Note: we would to calibrate the camera again if we change the camera optics (focus for example)
     def calibrate_camera(self):
-        WAIT_TIME = self.WAIT_TIME
+        wait_time = self.wait_time
         chessb_col = self.chessb_col
         chessb_row = self.chessb_row
         load_image = self.load_image
@@ -40,6 +42,7 @@ class ArucoInterface(object):
         images = glob.glob(load_image)
 
         for fname in images:
+            print("loading %s" % fname)
             img = cv2.imread(fname)
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -56,7 +59,7 @@ class ArucoInterface(object):
                 # Draw and display the corners
                 img = cv2.drawChessboardCorners(img, (chessb_col,chessb_row), corners2,ret)
                 cv2.imshow('img',img)
-                cv2.waitKey(WAIT_TIME)
+                cv2.waitKey(wait_time)
 
         cv2.destroyAllWindows()
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
@@ -67,25 +70,19 @@ class ArucoInterface(object):
         # note you *release* you don't close() a FileStorage object
         cv_file.release()
         
-    # Just extracts the values we have obtained with the calibrateCamera
-    def extract_cameracalib(self):
 
+    def extract_calibration(self):
+        ''' Gets the the camera and distortion matrix from the calibrate_camera method. By reading the yaml file.'''
         cv_file = cv2.FileStorage("calib_images/test.yaml", cv2.FILE_STORAGE_READ)
-
-        print 'Type from read file:', type(cv_file)
-
-        #note we also have to specify the type to retrieve other wise we only get a
+        #print 'Type from read file:', type(cv_file)
+        # Note we also have to specify the type to retrieve other wise we only get a
         # FileNode object back instead of a matrix
 
         camera_matrix = cv_file.getNode("camera_matrix").mat()
         dist_matrix = cv_file.getNode("dist_coeff").mat()
-
-        print type(camera_matrix)
-
-        print("camera_matrix : ", camera_matrix.tolist())
-        print("dist_matrix : ", dist_matrix.tolist())
-
         cv_file.release()
+        print "Camera Matrix: %s", camera_matrix 
+        return camera_matrix, dist_matrix
 
     # We choose an image to draw the axis served as reference to determine the pose
     def estimate_pose(self):
@@ -161,50 +158,19 @@ class ArucoInterface(object):
 
         cv2.destroyAllWindows()
 
-    # Tracking the Aruco with a video stream
     def track_aruco(self):
+        ''' Tracks the ArUco Marker in real time. '''
         chessb_col = self.chessb_col
         chessb_row = self.chessb_row
         load_image = self.load_image
         criteria = self.criteria
-        marker_size = 0.045
+        marker_size = self.marker_size
 
-        # Get video stream from camera source, in our case 1 is the argument because we're using
-	# a laptop and there are 2 possible video streams (webcam and the logitech camera)
+
+        # Get video stream from camera source
         cap = cv2.VideoCapture(1)
-
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        objp = np.zeros((chessb_row*chessb_col,3), np.float32)
-        objp[:,:2] = np.mgrid[0:chessb_col,0:chessb_row].T.reshape(-1,2)
-
-        # Arrays to store object points and image points from all the images.
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
-
-        # Read all images
-        images = glob.glob(load_image)
-
-        for fname in images:
-            img = cv2.imread(fname)
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-            # Find the chess board corners
-            ret, corners = cv2.findChessboardCorners(gray, (chessb_col,chessb_row),None)
-
-            # If found, add object points, image points (after refining them)
-            if ret == True:
-                objpoints.append(objp)
-
-                corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-                imgpoints.append(corners2)
-
-                # Draw and display the corners
-                img = cv2.drawChessboardCorners(img, (chessb_col,chessb_row), corners2,ret)
-            #print "entrou1"
-
-        # We are again obtaining the parameters, later im gonna fix this so it is more optmized
-        # The Rvecs and Tvecs in this case are not our rotational and translation parameters to determine the pose!
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        # Getting the calibrated parameters
+        camera_matrix, dist_matrix = self.extract_calibration()
 
         while (True):
             # Getting a frame from video stream
@@ -220,34 +186,34 @@ class ArucoInterface(object):
 
             # Lists of ids and the corners belonging to each id
             corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-
-            font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
-            # print "entrou3"
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
             # Checks before if all the values on the matrix are different than None
             if np.all(ids != None):
                 # Estimate pose of each marker and return the values rvet and tvec, NOTE THAT those are DIFFERENT from camera coefficents
                 # The second parameter is the size of the marker in meters
                 # The length of these vectors are just one, meaning they have 3 columns but just one row! And they store the real pose value
-                rvec, tvec,_ = aruco.estimatePoseSingleMarkers(corners[0], marker_size, mtx, dist)
+                rvec, tvec,_ = aruco.estimatePoseSingleMarkers(corners[0], marker_size, camera_matrix, dist_matrix)
+                #print rvec, tvec
                 #(rvec-tvec).any() # get rid of that nasty numpy value array error
                 #print 'Rotation Vector: ', rvec
-                #print 'Translation Vector:', tvec
+                print 'Translation Vector:', tvec
+                # Only uncomment these two following lines when using with ROS!
+                #if (rvec.any() != 0) or (tvec.any() != 0):
+                #    break
+                aruco.drawAxis(frame, camera_matrix, dist_matrix, rvec[0], tvec[0], 0.1)
+                # Drawing a square on the identified marker
+                aruco.drawDetectedMarkers(frame, corners)
 
-                # Found the pose, now just quit the loop and send the command to the robot to stop moving as well!
-               
-                aruco.drawAxis(frame, mtx, dist, rvec[0], tvec[0], 0.1) #Draw Axis
-                aruco.drawDetectedMarkers(frame, corners) #Draw A square around the markers
-
-
-                ###### DRAW ID #####
+                ###### Draw ID on the screen #####
                 cv2.putText(frame, "Id: " + str(ids), (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
-		print "Translation vector: ", tvec
+
 
             # Display the resulting frame
-            cv2.imshow('frame',frame)
+            cv2.imshow('ArucoDetection', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
         cap.release()
         cv2.destroyAllWindows()
         _rvec = np.copy(rvec)
@@ -255,5 +221,10 @@ class ArucoInterface(object):
         return _rvec, _tvec
 
 
-arucao = ArucoInterface()
-arucao.track_aruco()
+def main():
+    arucao = ArucoInterface()
+    # Run calibrate_camera before actually running track_aruco
+    # So we have our camera matrix and distortion coefficients
+    #arucao.calibrate_camera()
+    arucao.track_aruco()
+main()
--- henrique/src/calib_images/convert.py
+++ henrique/src/calib_images/convert.py
@@ -2,8 +2,11 @@ from glob import glob
 import cv2 
 
 # Drag this program to where you want to convert your files and then just run it on the terminal!
+def convert():
+    pngs = glob('./*.png')
+    for i in pngs:
+        img = cv2.imread(i)
+        cv2.imwrite(i[:-3] + 'jpg', img)
 
-pngs = glob('./*.png')
-for i in pngs:
-    img = cv2.imread(i)
-    cv2.imwrite(i[:-3] + 'jpg', img)
+if __name__ == "__main__":
+    convert()
